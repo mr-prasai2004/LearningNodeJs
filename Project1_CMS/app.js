c3ac9e12e0871bd316bb8791f656953e84e65137
Aayush Prasai <gayatriprasai@gmail.com>
2025-06-19 07:41:52 +0545

    AllBlog Page createed

--- Project1_CMS/app.js
+++ Project1_CMS/app.js
@@ -6,17 +6,14 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/',(req,res)=>{
-    res.send("Welcome Aayush")
+app.get('/allblogs',(req,res)=>{
+    res.render("allBlogs")
 })
 
 app.get('/addblog',(req,res)=>{
     res.render('addBlog')
 })
 
-app.get('/editblog',(req,res)=>{
-    res.render('editBlog')
-})
 
 app.listen(3000,()=>{
     console.log("server is running in port 3000");
